6f5fed5201c87efb2ef2eb1d54b09afb56604c84
AT <athinat690@gmail.com>
2023-12-12 16:35:20 +0200

    topmovies section done

--- components/TopMovies.tsx
+++ components/TopMovies.tsx
@@ -1,33 +1,38 @@
+import { topMovies } from "@/constants";
 import React from "react";
 
-const MovieCard = () => (
-  <div className="flex-center flex-col m-3 gap-10">
-    <p className="h1 opacity-50 text-white font-redHatDisplay">01</p>
-    <div
-      className="w-[200px] h-[180px] bg-black rounded-xl flex-end"
-      //   style={{
-      //     backgroundImage: `url(https://image.tmdb.org/t/p/original/${poster})`,
-      //     backgroundSize: "cover",
-      //     backgroundPosition: "center",
-      //     height: "100%",
-      //   }}
-    >
-      <div className="w-[70px] h-[30px] rounded-xl light_red border_radius flex-center">
-        <p className="p2 text-white font-inter">TOP 1</p>
+const MovieCard = ({ number, ranking, title }: any) => (
+  <div className="flex-center flex-col m-3 gap-10 md:flex-row">
+    <p className="h1 opacity-50 text-white font-redHatDisplay">{number}</p>
+    <div className="flex-center flex-col gap-5">
+      <div
+        className="w-[200px] h-[180px] bg-black rounded-xl flex-end"
+        //   style={{
+        //     backgroundImage: `url(https://image.tmdb.org/t/p/original/${poster})`,
+        //     backgroundSize: "cover",
+        //     backgroundPosition: "center",
+        //     height: "100%",
+        //   }}
+      >
+        <div className="w-[70px] h-[30px] rounded-xl light_red border_radius flex-center">
+          <p className="p2 text-white font-inter">{ranking}</p>
+        </div>
       </div>
-    </div>
-    <div className="h-full">
-      <p className="font-inter h5 text-white text-center">
-        Some Shit title Here
-      </p>
+      <div className="h-full">
+        <p className="font-inter h5 text-white text-center">{title}</p>
+      </div>
     </div>
   </div>
 );
 
 const TopMovies = () => {
   return (
-    <div>
-      <MovieCard />
+    <div className="w-full flex-center flex-col">
+      <div className="grid grid-cols-1 md:grid-cols-2 items-center justify-center gap-5 m-5 xl:grid-cols-3">
+        {topMovies.map((item) => (
+          <MovieCard key={item.title} {...item} />
+        ))}
+      </div>
     </div>
   );
 };
--- constants/index.ts
+++ constants/index.ts
@@ -123,3 +123,36 @@ export const showCase: IShowCase[] = [
     title_2: "Again",
   },
 ];
+
+export const topMovies = [
+  {
+    number: "01",
+    ranking: "TOP 1",
+    title: "The Shawshank Redemption",
+  },
+  {
+    number: "02",
+    ranking: "TOP 2",
+    title: "The Shawshank Redemption",
+  },
+  {
+    number: "03",
+    ranking: "TOP 3",
+    title: "The Shawshank Redemption",
+  },
+  {
+    number: "04",
+    ranking: "TOP 4",
+    title: "The Shawshank Redemption",
+  },
+  {
+    number: "05",
+    ranking: "TOP 5",
+    title: "The Shawshank Redemption",
+  },
+  {
+    number: "06",
+    ranking: "TOP 6",
+    title: "The Shawshank Redemption",
+  },
+];
