d4c0d3c63b9b2d82a9817fbb857465a9675d4efc
AT <athinat690@gmail.com>
2023-12-16 19:58:37 +0200

    Update the Contact page with a new background gradient and add a navigation bar.

--- app/(root)/contact/page.tsx
+++ app/(root)/contact/page.tsx
@@ -1,7 +1,23 @@
+import Navbar from "@/components/navigation/Navbar";
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="contact_bg_gradient">
+      <Navbar bgColor="dark" />
+      <div className="m-3">
+        <div className="flex-center flex-col h-[80vh]">
+          <p className="p3 text-white md:p2">Reach out!</p>
+          <h2 className="h2 font-redHatDisplay red_text">Contact Us</h2>
+          <p className="p2 text-white text-center md:p1">
+            Tell us about your experience and questions. We gladly help you to
+            give the best solution.
+          </p>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
